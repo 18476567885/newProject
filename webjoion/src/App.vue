b064d6a2c79543a83a7afda572b0a9c875fdad3e
18476567885 <13727699@qq.com>
2021-02-24 00:57:31 +0800

    web

---
+++ webjoion/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+   <newProject1></newProject1>
+  </div>
+</template>
+
+<script>
+import newProject1 from './components/newProject1.vue'
+
+export default {
+  name: 'App',
+  components: {
+    newProject1
+  }
+}
+</script>
+
+<style>
+
+</style>
